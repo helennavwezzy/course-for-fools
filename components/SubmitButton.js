6b8c642185c2f56e303f30341091e9f889b3746b
Cảnh Văn <90491868+helennavwezzy@users.noreply.github.com>
2025-03-22 14:15:39 +0700

    Update SubmitButton.js

--- components/SubmitButton.js
+++ components/SubmitButton.js
@@ -1,2 +1,10 @@
-components/
-  - SubmitButton.js
+export default function SubmitButton({onClick}) {
+    return (
+        <button
+            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-700 transition font-bold"
+            onClick={onClick}
+        >
+            Submit
+        </button>
+    )
+}
